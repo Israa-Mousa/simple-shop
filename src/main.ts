6d3c344d590eaf0ec4e06c0766842035e8f7b103
Israa-Mousa <israa.mazaraa@yahoo.com>
2026-01-03 23:24:43 +0200

    end session

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './interceptors/loggin.interceptor';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { HttpExceptionFilter, ImageKitExceptionFilter } from './exceptions/exception';
 BigInt.prototype.toJSON = function () {
   return this.toString();
 };
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+app.useGlobalInterceptors(new LoggingInterceptor(),new ResponseInterceptor(),);
+ 
+app.useGlobalFilters(new HttpExceptionFilter(),new ImageKitExceptionFilter);
+await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/types/util.types.ts
+++ src/types/util.types.ts
@@ -1,3 +1,4 @@
+import { HttpStatus } from "@nestjs/common";
 import { Prisma } from "generated/prisma";
 
 export type PaginationQueryType = {
@@ -19,3 +20,19 @@ export type PaginatedResult<T> = {
 //big  int 
 
 export type TransactionClient = Prisma.TransactionClient;
+
+
+type ApiSuccessResponse<T> = {
+  success: true;
+  data: T | T[];
+} & Partial<PaginationResponseMeta>;
+
+export type ApiErrorResponse = {
+  success: false;
+  message: string;
+  timestamp: string;
+  statusCode: HttpStatus;
+  path: string;
+  fields?: { field: string; message: string }[];
+};
+export type UnifiedApiResponse<T> = ApiSuccessResponse<T>;
